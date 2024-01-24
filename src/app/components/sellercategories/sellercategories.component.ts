53be0c4f1a0dc7ba6eb10ee86c7d4dbe09b7842c
MenukaRanasinghe <88645663+MenukaRanasinghe@users.noreply.github.com>
2024-01-24 12:22:50 +0530

    sellerCategory UI done

--- src/app/components/sellercategories/sellercategories.component.ts
+++ src/app/components/sellercategories/sellercategories.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CategoryService } from '../category.service';
 
 @Component({
   selector: 'app-sellercategories',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./sellercategories.component.scss']
 })
 export class SellercategoriesComponent {
+  categories: any[] = [];
 
+  constructor(private categoryService: CategoryService) {}
+
+  ngOnInit(): void {
+    this.categoryService.getCategories().subscribe(data => {
+      this.categories = data;
+    });
+  }
+
+  addCategory(): void {
+    console.log('Add Category clicked');
+  }
+
+  updateCategory(categoryId: number): void {
+    console.log(`Update category clicked for ID: ${categoryId}`);
+  }
+
+  deleteCategory(categoryId: number): void {
+    console.log(`Delete category clicked for ID: ${categoryId}`);
+  }
 }
